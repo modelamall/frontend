44c095159237ce0e5f500018e000bf4663c8fdcf
Abdulrahman Albarghouth <58550474+Abdulrahman-Albarghouth@users.noreply.github.com>
2023-02-11 17:21:07 +0300

    product

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Suspense } from 'react'
 import { Route, Routes } from "react-router-dom";
 
 const Home = React.lazy(() => import("./pages/Home"));
+const SingelProduct = React.lazy(() => import("./components/Products/SingelProduct"));
 const NavBar = React.lazy(() => import("./components/NavBar/NavBar"));
 
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Suspense><Home /></Suspense>} />
+      <Route path="/product/:id" element={<Suspense><SingelProduct /></Suspense>} />
       <Route path="/navBar" element={<Suspense><NavBar/></Suspense>} />
     </Routes>
   );
