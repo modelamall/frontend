ea23cd571f12e58d665eb9efc1ee467ee112f1ac
Feras Jobeir <fjobeir@gmail.com>
2023-02-15 12:42:19 +0300

    dashboard routes

--- src/App.js
+++ src/App.js
@@ -14,12 +14,15 @@ const Profile = React.lazy(() => import("./pages/Profile"));
 function App() {
   return (
     <Routes>
-      <Route path="/product" element={<Wrapper><Suspense><Home /></Suspense></Wrapper>} />
-      <Route path="/product/:id" element={<Wrapper><Suspense><SingelProduct /></Suspense></Wrapper>} />
-      <Route path="/navBar" element={<Wrapper><Suspense><NavBar/></Suspense></Wrapper>} />
-      <Route path="/signup" element={<Wrapper><Suspense><SignUp/></Suspense></Wrapper>} />
-      <Route path="/signin" element={<Wrapper><Suspense><SignIn/></Suspense></Wrapper>} />
-      <Route path="/profile" element={<Wrapper><Suspense><Profile/></Suspense></Wrapper>} />
+      <Route path="/product" element={<Suspense><Home /></Suspense>} />
+      <Route path="/product/:id" element={<Suspense><SingelProduct /></Suspense>} />
+      <Route path="/navBar" element={<Suspense><NavBar/></Suspense>} />
+      <Route path="/signup" element={<Suspense><SignUp/></Suspense>} />
+      <Route path="/signin" element={<Suspense><SignIn/></Suspense>} />
+      <Route path="/profile" element={<Suspense><Profile/></Suspense>} />
+      <Route path="dashboard/*">
+
+      </Route>
     </Routes>
   );
 }
