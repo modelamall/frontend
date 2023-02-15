32cbfb9109a81897ba33748f51afa85aab4b82c4
Abdulrahman Albarghouth <58550474+Abdulrahman-Albarghouth@users.noreply.github.com>
2023-02-15 13:20:23 +0300

    edit

--- src/components/Products/SingelProduct.js
+++ src/components/Products/SingelProduct.js
@@ -37,7 +37,7 @@ const SingelProduct = () => {
           {/* Image gallery */}
           <Tab.Group as="div" className="flex flex-col-reverse">
             {/* Image selector */}
-            <div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
+            <div className="mx-auto mt-6 w-full max-w-2xl sm:block lg:max-w-none">
               <Tab.List className="grid grid-cols-4 gap-6">
                 {!(responseData?.data?.ProductVariations[selectedProduct]?.Pictures.length > 0) && responseData?.data?.Pictures.map((image) => (
                   <Tab
