bd2910aeae62dfd1a8df04522b468125f0ca2967
Feras Jobeir <fjobeir@gmail.com>
2023-02-15 12:44:14 +0300

    show navbar always

--- src/App.js
+++ src/App.js
@@ -13,10 +13,11 @@ const Profile = React.lazy(() => import("./pages/Profile"));
 
 function App() {
   return (
+    <>
+    <NavBar />
     <Routes>
       <Route path="/product" element={<Suspense><Home /></Suspense>} />
       <Route path="/product/:id" element={<Suspense><SingelProduct /></Suspense>} />
-      <Route path="/navBar" element={<Suspense><NavBar/></Suspense>} />
       <Route path="/signup" element={<Suspense><SignUp/></Suspense>} />
       <Route path="/signin" element={<Suspense><SignIn/></Suspense>} />
       <Route path="/profile" element={<Suspense><Profile/></Suspense>} />
@@ -24,6 +25,7 @@ function App() {
 
       </Route>
     </Routes>
+    </>
   );
 }
 
