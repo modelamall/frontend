fee17497d79faea25fc59e5b9d5253acc7414134
Abdulrahman Albarghouth <58550474+Abdulrahman-Albarghouth@users.noreply.github.com>
2023-02-18 15:27:13 +0300

    product

--- src/pages/Dashboard/DashboardLayout.js
+++ src/pages/Dashboard/DashboardLayout.js
@@ -146,7 +146,6 @@ export default function DashboardLayout({ navigation = [], children }) {
                                         key={i}
                                         to={item.to}
                                         className={({ isActive }) => {
-                                            console.log(isActive)
                                             return classNames(
                                                 isActive ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                                                 'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
