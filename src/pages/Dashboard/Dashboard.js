c7c44c271bed07b9cfef17e16cc8b0147eadeff6
M Eyad Masri <mahmoudiyad1996@gmail.com>
2023-02-19 20:59:31 +0300

    stroe sign

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -3,11 +3,9 @@ import { Route, Routes } from "react-router-dom";
 import AdminDashboard from "../../components/Dashboard/AdminDashboard";
 import StoreDashboard from "../../components/Dashboard/StoreDashboard";
 import { AuthContext } from "../../context/AuthContext";
-<<<<<<< HEAD
-import StoreSignIn from "./StoreSignIn";
-=======
 const AdminSignIn = React.lazy(() => import("./AdminSignIn"));
->>>>>>> 4c7bd9c6e5e2487aa5fd7081bb4521f33143124a
+const StoreSignIn = React.lazy(() => import("./StoreSignIn"));
+
 
 const Dashboard = () => {
   const { dashboardUser, dashboardToken } = useContext(AuthContext);
@@ -28,8 +26,9 @@ const Dashboard = () => {
       )}
       {!dashboardToken && (
         <Routes>
-          <Route path="*" element={<StoreSignIn/>} />
           <Route path="/admin" element={<AdminSignIn />} />
+          <Route path="*" element={<StoreSignIn/>} />
+
         </Routes>
       )}
     </>
