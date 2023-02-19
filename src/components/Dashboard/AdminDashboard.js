7350d43c8570a0b05d57ec9ad33fa3b4c2933046
M Eyad Masri <mahmoudiyad1996@gmail.com>
2023-02-19 21:33:42 +0300

    admin

--- src/components/Dashboard/AdminDashboard.js
+++ src/components/Dashboard/AdminDashboard.js
@@ -10,15 +10,19 @@ const menuItems = [
     { name: 'Dashboard', to: '/dashboard/dashboard', icon: HomeIcon },
     { name: 'Orders', to: '/dashboard/orders', icon: UsersIcon },
     { name: 'Profile', to: '/dashboard/profile', icon: FolderIcon },
+    { name: 'Sign Out', to: '/dashboard/signout', icon: ArrowLeftOnRectangleIcon },
+
 ]
 
 const AdminDashboard = () => {
+    const DashboardSignOut = React.lazy(() => import("./DashboardSignOut"));
+
     return (
         <DashboardLayout navigation={menuItems}>
             <Routes>
                 <Route path="orders" element={<>Orders</>} />
                 <Route path="profile" element={<PersonalInfo/>} />
-                <Route path="signout" element={<>sign out</>} />
+                <Route path="signout" element={<Suspense><DashboardSignOut/></Suspense>} />
                 <Route path="*" element={<>...</>} />
             </Routes>
         </DashboardLayout>
