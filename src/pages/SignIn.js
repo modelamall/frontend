c02e36e8ccfb233873c85c90802b3e8142b1c7e8
M Eyad Masri <mahmoudiyad1996@gmail.com>
2023-02-22 15:53:41 +0300

    edits

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -53,7 +53,7 @@ const SignIn = () => {
 
   return (
     <>
-      <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-[url('https://tailwindui.com/img/beams-basic-transparent.png')]">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
